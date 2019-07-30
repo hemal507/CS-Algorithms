91b2b1f8177997058f4324e75f47f7231f814b7b
hemal507 <hchowdam@gmail.com>
2019-07-30 20:41:16 +0530

    Add files via upload

---
+++ test_isLuckyNumberImproved.py
@@ -0,0 +1,7 @@
+import isLuckyNumber
+def test_isLuckyNumberImprv1() :
+        assert isLuckyNumber.isLucky_improved(1230) == True
+
+def test_isLuckyNumberImprv2() :
+        assert isLuckyNumber.isLucky_improved(3457) == False
+
