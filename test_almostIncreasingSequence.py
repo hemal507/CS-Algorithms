ca2677ad5e0bb907da1b320d0a072e7e89487cbe
hemal507 <hchowdam@gmail.com>
2019-08-01 07:20:35 +0530

    Add files via upload

---
+++ test_almostIncreasingSequence.py
@@ -0,0 +1,8 @@
+import almostIncreasingSequence
+
+def test_case1() :
+	assert almostIncreasingSequence.almostIncreasingSequence2([[1, 3, 2]]) == True
+
+def test_case2() :
+	assert almostIncreasingSequence.almostIncreasingSequence2[[1, 3, 2, 1]]) == False
+
