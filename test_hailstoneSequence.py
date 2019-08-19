17a119be4189c237da169801e1681957ae04c88d
hemal507 <hchowdam@gmail.com>
2019-08-19 20:36:28 +0530

    Add files via upload

---
+++ test_hailstoneSequence.py
@@ -0,0 +1,10 @@
+import hailstoneSequence
+
+def test_case1() :
+	assert hailstoneSequence.hailstoneSequence(5) == 5
+
+def test_case2() :
+	assert hailstoneSequence.hailstoneSequence(27) == 111
+
+def test_case3() :
+	assert hailstoneSequence.hailstoneSequence(16) == 4
