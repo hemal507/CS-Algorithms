db5f5f384c7706b9e3430310c1f4aff5586e0dd8
hemal507 <hchowdam@gmail.com>
2019-09-13 12:27:24 +0530

    Update arrayReplace.py

--- arrayReplace.py
+++ arrayReplace.py
@@ -1,3 +1,8 @@
+#using lambda
+def arrayReplace(p, q, r):
+    return map(lambda x : r if x == q else x, p)
+
+#using list comprehension
 def arrayReplace(p, q, r):
     return [r if i == q else i for i in p]
 
