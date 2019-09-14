fd8e762acd1255c66545d4037ea537b4534092ea
hemal507 <hchowdam@gmail.com>
2019-09-14 21:17:44 +0530

    Update imageTruncation.py

--- imageTruncation.py
+++ imageTruncation.py
@@ -1,3 +1,7 @@
 def imageTruncation(image, threshold):
     return [ [x if x <= threshold else threshold for x in a  ] for a in image ]
 
+# alternative solution
+def imageTruncation(i, j):
+    return [ [[x,j][x>j] for x in a  ] for a in i ]
+
