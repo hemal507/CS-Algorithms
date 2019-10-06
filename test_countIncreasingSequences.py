860651d620d343e3f4457c03554e11bff069c72b
hemal507 <hchowdam@gmail.com>
2019-10-06 16:32:02 +0530

    Add files via upload

---
+++ test_countIncreasingSequences.py
@@ -0,0 +1,7 @@
+import countIncreasingSequences
+
+def test_case1() :
+	assert countIncreasingSequences.countIncreasingSequences(2,3) == 3
+
+def test_case2() :
+        assert countIncreasingSequences.countIncreasingSequences(1,4) == 4
