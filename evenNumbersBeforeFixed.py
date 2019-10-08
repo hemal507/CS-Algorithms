fbed311355230a850bad9e14b8571ac49e67066b
hemal507 <hchowdam@gmail.com>
2019-10-08 10:56:23 +0530

    Add files via upload

--- evenNumbersBeforeFixed.py
+++ evenNumbersBeforeFixed.py
@@ -6,4 +6,8 @@ def evenNumbersBeforeFixed(s, f):
         r += i % 2 == 0 
     return -1
 
+from itertools import takewhile
+def evenNumbersBeforeFixed2(s,f) :
+	r=len(filter(lambda x: x%2==0 , (takewhile(lambda x : x!=f , s))))
+	return -1 if r==0 else r
 
