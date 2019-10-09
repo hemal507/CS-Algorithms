0acdb06fdb27d74a52c2a569b61a98bdafa45fa6
hemal507 <hchowdam@gmail.com>
2019-10-09 21:51:20 +0530

    Add files via upload

---
+++ higherVersion.py
@@ -0,0 +1,11 @@
+def higherVersion(v1, v2):
+        l1 = v1.split(".")
+        l2 = v2.split(".")
+        for i in range(len(l1)) :
+                if int(l1[i]) < int(l2[i]) :
+                        return False
+                elif int(l1[i]) > int(l2[i]) :
+                        return True
+        return False
+
+
