305b4b54ca4931c86ff61e4cd16f930cc28f1c58
hemal507 <hchowdam@gmail.com>
2019-10-14 17:54:01 +0530

    Add files via upload

---
+++ test_minimalMultiple.py
@@ -0,0 +1,10 @@
+import minimalMultiple
+
+def test_case1() :
+	assert minimalMultiple.minimalMultiple(5,12) == 15
+
+def test_case2() :
+        assert minimalMultiple.minimalMultiple(1,239) == 239
+
+def test_case3() :
+        assert minimalMultiple.minimalMultiple(15,1) == 15
