da8192ff85ea1e33395fd7663fa50139e6ad6e80
hemal507 <hchowdam@gmail.com>
2019-10-17 18:57:18 +0530

    Add files via upload

---
+++ test_sumOfMultiples.py
@@ -0,0 +1,10 @@
+import sumOfMultiples
+
+def test_case1() :
+	assert sumOfMultiples.sumOfMultiples(7,2) == 12 
+
+def test_case2() :
+	assert sumOfMultiples.sumOfMultiples(4,1) == 10
+
+def test_case3() :
+	assert sumOfMultiples.sumOfMultiples(5,2) == 6
