b0c2c06c3835e7685b62e34534336bd9742dabf6
hemal507 <hchowdam@gmail.com>
2019-10-18 20:31:20 +0530

    Add files via upload

---
+++ test_smallestNumber.py
@@ -0,0 +1,10 @@
+import smallestNumber
+
+def test_case1():
+	assert smallestNumber.smallestNumber(1) == 0
+
+def test_case2():
+        assert smallestNumber.smallestNumber(2) == 10
+
+def test_case3():
+        assert smallestNumber.smallestNumber(3) == 100
