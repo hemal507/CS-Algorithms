434be48b58fb5537883c6f83640caf3555ef05f5
hemal507 <hchowdam@gmail.com>
2019-10-30 21:59:00 +0530

    Add files via upload

---
+++ test_digitDistanceNumber.py
@@ -0,0 +1,14 @@
+import digitDistanceNumber
+
+def test_case1() :
+	assert digitDistanceNumber.digitDistanceNumber(2744) == 530
+
+def test_case2() :
+        assert digitDistanceNumber.digitDistanceNumber(330) == 3
+
+def test_case3() :
+        assert digitDistanceNumber.digitDistanceNumber(333) == 0
+
+def test_case4() :
+        assert digitDistanceNumber.digitDistanceNumber(19) == 8 
+
