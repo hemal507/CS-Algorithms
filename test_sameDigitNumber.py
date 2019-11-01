158f1921333c973ce7510cac8c33020e3739626e
hemal507 <hchowdam@gmail.com>
2019-11-01 17:44:19 +0530

    Add files via upload

--- test_sameDigitNumber.py
+++ test_sameDigitNumber.py
@@ -1,9 +1,22 @@
 import sameDigitNumber
-import pytest
 
-@pytest.fixture(scope='module')
 def test_case1() :
-    assert sameDigitNumber.sameDigitNumber(1111) == True
+	assert sameDigitNumber.sameDigitNumber(1111) == True
 
 def test_case2() :
-    assert sameDigitNumber.sameDigitNumber(1121) == False
+        assert sameDigitNumber.sameDigitNumber(1122) == False
+
+def test_case3() :
+        assert sameDigitNumber.sameDigitNumber(2222222) == True
+
+def test_case4() :
+        assert sameDigitNumber.sameDigitNumber(11112) == False
+
+def test_case5() :
+        assert sameDigitNumber.sameDigitNumber(54444) == False
+
+def test_case6() :
+        assert sameDigitNumber.sameDigitNumber(0) == True
+
+def test_case7() :
+        assert sameDigitNumber.sameDigitNumber(99) == True
