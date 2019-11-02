93609a68df70dc045bfcca4a572e2548f63f7936
hemal507 <hchowdam@gmail.com>
2019-11-02 10:09:53 +0530

    Add files via upload

---
+++ fibonacciSum.py
@@ -0,0 +1,14 @@
+def fibonacciSum(n):
+    i = [1]
+    j = 2
+    r=[]
+    while j <= n :
+	i.insert(0,j)
+	j += i[1]
+    for f in i:
+        if f <= n:
+            r.insert(0,f)
+            n -= f
+    return r
+
+print(fibonacciSum(20))
