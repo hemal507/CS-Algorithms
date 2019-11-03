0642a6c3a8aff7bbcdc6673056a70717df568012
hemal507 <hchowdam@gmail.com>
2019-11-03 20:44:54 +0530

    Update fibonacciSeries.py

--- fibonacciSeries.py
+++ fibonacciSeries.py
@@ -11,3 +11,19 @@ def fibonacciGenerator(num,break_point1,break_point2) :
 #print(fibonacci(9))
 #print(fibonacciGenerator(9,0,1))
 
+# using memoization
+fib_cache = {}
+def fib(n) :
+        if n in fib_cache :
+                return fib_cache[n]
+
+        if n == 1 :
+                return 1
+        elif n == 2 :
+                return 1
+        elif n > 2 :
+                value = fib(n-1) + fib(n-2)
+
+        fib_cache[n] = value
+        return value
+
