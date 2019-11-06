dfc99aa13304077517ecf2edce8d8c42c45932b6
hemal507 <hchowdam@gmail.com>
2019-11-06 17:56:50 +0530

    Update divNumber.py

--- divNumber.py
+++ divNumber.py
@@ -2,5 +2,8 @@ def divNumber(k, l, r):
     t = 0
     for i in range(l,r+1) :
 	t +=  len([x for x in range(1,i+1) if i % x == 0]) == k
-    return t            
+    return t    
 
+def divNumber(k, l, r):
+    g=range
+    return sum([sum([i % x == 0 for x in g(1,i+1)]) == k  for i in g(l,r+1)])
