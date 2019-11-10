4dcded2ec5dcc7cca80353c8afd4540e64e99b02
hemal507 <hchowdam@gmail.com>
2019-11-10 17:31:03 +0530

    Add files via upload

---
+++ test_isSum.py
@@ -0,0 +1,26 @@
+import isSum
+
+def test_case1() :
+	assert isSum.isSum(10) == True
+
+def test_case1() :
+        assert isSum.isSum(11) == False
+
+def test_case1() :
+        assert isSum.isSum(100) == False
+
+def test_case1() :
+        assert isSum.isSum(42) == True
+
+def test_case1() :
+        assert isSum.isSum(110) == False
+
+def test_case1() :
+        assert isSum.isSum(45) == True
+
+def test_case1() :
+        assert isSum.isSum(55) == True
+
+def test_case1() :
+        assert isSum.isSum(1) == True
+
