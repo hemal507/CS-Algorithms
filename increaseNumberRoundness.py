a6e6caef0d20e6dcf51105a610d3b6ecd2897def
hemal507 <hchowdam@gmail.com>
2019-11-15 09:30:14 +0530

    Update increaseNumberRoundness.py

--- increaseNumberRoundness.py
+++ increaseNumberRoundness.py
@@ -1,7 +1,5 @@
-import re
 def increaseNumberRoundness(n) :
-	l=[x for x,y in enumerate(`n`) if y == '0']
-	if len(l) == 0 or (len(l) > 1 and l[0] == l[1]-1) :
-		return False
-	return True
-		
+        l=[x for x,y in enumerate(`n`) if y == '0']
+        if len(l) == 0 or (len(l) > 1 and l[0] == l[1]-1) or (len(l) == 1 and `n`.index('0') == len(`n`)-1) :
+                return False
+        return True
