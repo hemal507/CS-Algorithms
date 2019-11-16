fa31f433427d995b5933eb78db3a85884ddc175a
hemal507 <hchowdam@gmail.com>
2019-11-16 20:43:11 +0530

    Update increaseNumberRoundness.py

--- increaseNumberRoundness.py
+++ increaseNumberRoundness.py
@@ -3,3 +3,7 @@ def increaseNumberRoundness(n) :
         if len(l) == 0 or (len(l) > 1 and l[0] == l[1]-1) or (len(l) == 1 and `n`.index('0') == len(`n`)-1) :
                 return False
         return True
+
+import re
+def increaseNumberRoundness(n) :
+        return bool(re.search('0[1-9]',`n`))
