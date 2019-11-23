8d894847a45c77dc038ae19effd1722fa8b6b80f
hemal507 <hchowdam@gmail.com>
2019-11-23 09:52:02 +0530

    Add files via upload

---
+++ test_factorialsProductTrailingZeros.py
@@ -0,0 +1,13 @@
+import factorialsProductTrailingZeros
+
+def test_case1() :
+	assert factorialsProductTrailingZeros.factorialsProductTrailingZeros(4,10) == 7
+
+def test_case1() :
+        assert factorialsProductTrailingZeros.factorialsProductTrailingZeros(10,15) == 13
+
+def test_case1() :
+        assert factorialsProductTrailingZeros.factorialsProductTrailingZeros(29,31) == 20
+
+def test_case1() :
+        assert factorialsProductTrailingZeros.factorialsProductTrailingZeros(5,5) == 1
