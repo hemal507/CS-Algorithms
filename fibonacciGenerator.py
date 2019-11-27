e32dd3b33ea39819f3169b1974bf096942406474
hemal507 <hchowdam@gmail.com>
2019-11-27 08:48:21 +0530

    Add files via upload

---
+++ fibonacciGenerator.py
@@ -0,0 +1,12 @@
+def fibonacciGenerator(n):
+    def fib():
+        last = (0, 1)
+        while True:
+            yield last[0]
+	    print(last)
+            last = last[0] + last[1], last[0]
+
+    gen = fib()
+    return [next(gen) for _ in range(n)]
+
+print(fibonacciGenerator(9))
