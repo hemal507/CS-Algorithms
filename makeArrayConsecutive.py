0b0ccb2b02a0a70a79c77d1fcbdce1fc2a7ca73a
hemal507 <hchowdam@gmail.com>
2019-11-30 18:02:02 +0530

    Update makeArrayConsecutive.py

--- makeArrayConsecutive.py
+++ makeArrayConsecutive.py
@@ -1,5 +1,4 @@
 def makeArrayConsecutive(sequence):
     return [x for x in range(min(sequence)+1, max(sequence)) if x not in sequence]
     
-
-
+makeArrayConsecutive = lambda sequence : [x for x in range(min(sequence)+1, max(sequence)) if x not in sequence]
