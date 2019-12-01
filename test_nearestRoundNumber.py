1d447a2610c24035539fef20706da13c689243b9
hemal507 <hchowdam@gmail.com>
2019-12-01 07:52:19 +0530

    Add files via upload

---
+++ test_nearestRoundNumber.py
@@ -0,0 +1,12 @@
+import nearestRoundNumber
+
+def test_case1() :
+	assert nearestRoundNumber.nearestRoundNumber(122) == 130
+
+def test_case2() :
+        assert nearestRoundNumber.nearestRoundNumber(100) == 100
+
+def test_case3() :
+        assert nearestRoundNumber.nearestRoundNumber(122903530409) == 122903530410
+
+
