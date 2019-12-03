55e1ef8d6ad73a2f7d3678de8634ed72b91bd59c
hemal507 <hchowdam@gmail.com>
2019-12-03 08:06:22 +0530

    Add files via upload

---
+++ test_maxZeros.py
@@ -0,0 +1,11 @@
+import maxZeros
+	
+def test_case1() :
+	assert maxZeros.maxZeros(9) == 2
+
+def test_case2() :
+        assert maxZeros.maxZeros(7) == 7
+
+def test_case3() :
+        assert maxZeros.maxZeros(127) == 5
+
