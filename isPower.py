9ea9da7aafd7ebba40270d4ace0fb26cd43bb951
hemal507 <hchowdam@gmail.com>
2019-12-05 17:28:15 +0530

    Add files via upload

---
+++ isPower.py
@@ -0,0 +1,22 @@
+'''
+def isPower(n):
+    if n == 1:
+        return True
+
+    for i in range(2, n):
+        if n % i == 0:
+            tmp = n
+            while n % i == 0:
+                n /= i
+            if n == 1:
+                return True
+            n = tmp
+    return False
+'''
+
+def isPower(n) :
+	r=range(n) 
+	return [True for x in r for y in r if x**y == n ] > [ ]
+
+print(isPower(72))
+print(isPower(100))
