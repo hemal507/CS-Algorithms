1be1050d9e0f5b7f1ab94fe3d85c6d41f5e83cd9
hemal507 <hchowdam@gmail.com>
2019-12-14 09:40:35 +0530

    Add files via upload

---
+++ test_numberOfOperations.py
@@ -0,0 +1,8 @@
+import numberOfOperations
+
+def test_case1() :
+	assert numberOfOperations.numberOfOperations(432, 2) == 4
+
+def test_case2() :
+        assert numberOfOperations.numberOfOperations(7, 14) == 1
+
