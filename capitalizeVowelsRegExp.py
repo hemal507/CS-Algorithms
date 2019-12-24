c8c38a60188834fce623263f245f22a8b4c7decd
hemal507 <hchowdam@gmail.com>
2019-12-24 15:42:31 +0530

    Add files via upload

--- capitalizeVowelsRegExp.py
+++ capitalizeVowelsRegExp.py
@@ -1,3 +1,5 @@
 def capitalizeVowelsRegExp(input):
 	return ''.join([x.upper() if x in 'aeiouy' else x for x in input ])
 
+import re
+capitalizeVowelsRegExp = lambda s : re.sub('[aeiouy]',lambda x : x.group(0).upper(),s)
