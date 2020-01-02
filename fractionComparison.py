fadf40f2055d8ff52aef9cd1a0f76132cdd8019f
hemal507 <hchowdam@gmail.com>
2020-01-02 09:50:32 +0530

    Add files via upload

--- fractionComparison.py
+++ fractionComparison.py
@@ -1,3 +1,7 @@
 def fractionComparison(a,b) :
     r=a[0]/float(b[0]) - a[1] / float(b[1])
     return '=' if r == 0 else '<' if r < 0 else '>'
+
+def fractionComparison(a,b) :
+    return '=><'[cmp(a[0]*1./b[0] , a[1]*1. / b[1])]
+
