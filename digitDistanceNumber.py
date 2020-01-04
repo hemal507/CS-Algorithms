754d03405c4f3e4585ccca5089fbb693f59f4fd8
hemal507 <hchowdam@gmail.com>
2020-01-04 19:28:18 +0530

    Update digitDistanceNumber.py

--- digitDistanceNumber.py
+++ digitDistanceNumber.py
@@ -1,4 +1,6 @@
 def digitDistanceNumber(n):
     return int(''.join([str(abs(int(x) - int(y))) for x,y in zip(`n`[1:],`n`[:-1])]))    
 
-
+def digitDistanceNumber(n):
+    i=map(int,`n`)
+    return reduce(lambda x,y : x*10+y,[abs(i[j+1]-i[j]) for j in range(len(i)-1)   ])
