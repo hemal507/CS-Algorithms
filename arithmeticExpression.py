12b25bfe95a3f5ef719881a4f1f5449f7bbf7f71
hemal507 <hchowdam@gmail.com>
2020-01-07 12:00:23 +0530

    Add files via upload

---
+++ arithmeticExpression.py
@@ -0,0 +1,6 @@
+arithmeticExpression = lambda a, b, c :  a + b == c or a - b == c or a * b == c or a*1. / b == c
+
+arithmeticExpression = lambda a, b, c :  c in [a + b, a - b, a * b, a*1. / b] 
+
+
+
