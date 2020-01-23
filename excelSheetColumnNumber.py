277c2351a839e2901dbfaa22fc26a596eb2340ac
hemal507 <hchowdam@gmail.com>
2020-01-23 21:49:42 +0530

    excelSheetColumnNumber

--- excelSheetColumnNumber.py
+++ excelSheetColumnNumber.py
@@ -1,9 +1,19 @@
+"""
+
 def excelSheetColumnNumber(s):
     v, j = 0, 0
     for i in s[::-1]:
         v += pow(26, j) * (ord(i) - 64)
         j += 1
     return v
+
+"""
+
+from functools import reduce
+
+
+def excelSheetColumnNumber(s):
+    return reduce(lambda x, y: x * 26 + ord(y) - 64, s, 0)
 
 
 print(excelSheetColumnNumber('AB'))
