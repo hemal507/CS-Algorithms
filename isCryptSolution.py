3baf49bd19b1ecb2286336fc1064b04954bd62c2
hemal507 <hchowdam@gmail.com>
2020-01-30 13:27:20 +0530

    isCryptSolution.py

--- isCryptSolution.py
+++ isCryptSolution.py
@@ -1,3 +1,4 @@
+"""
 def isCryptSolution(crypt, solution):
     c0,c1,c2 = [],[],[]
     for i in crypt[0] :
@@ -16,4 +17,17 @@ def isCryptSolution(crypt, solution):
         return False
 
     return int(''.join(c0) ) + int(''.join(c1)) == int(''.join(c2))
+"""
 
+
+def isCryptSolution(c, s):
+    r = []
+    for x in c:
+        r.append([z[1] for y in x for z in s if z[0] == y])
+    if any([x[0] == '0' for x in r]) and len(r[-1]) > 1:
+        return False
+    return sum(map(int, [''.join(x) for x in r[:2]])) == int(''.join(r[-1]))
+
+
+print(isCryptSolution(['SEND', 'MORE', 'MONEY'],
+                      [['O', '0'], ['M', '1'], ['Y', '2'], ['E', '5'], ['N', '6'], ['D', '7'], ['R', '8'], ['S', '9']]))
