cdc568b50db19f2c6e4bfc31407847818ea5a6e7
hemal507 <hchowdam@gmail.com>
2020-02-02 22:57:48 +0530

    surpasserCount.py

--- surpasserCount.py
+++ surpasserCount.py
@@ -1,5 +1,19 @@
+"""
 def surpasserCount(a):
     return sum([y > a[x] for x in range(len(a)) for y in a[x + 1:]]) % 1000000007
+"""
+
+import numpy
 
 
-print(surpasserCount([1, 2, 3, 4, 5]))
+def surpasserCount(a):
+    a = numpy.r_[a]
+    print(type(a))
+    r = 0
+    for i in a:
+        a = a[1:]
+        r += (a > i).sum()
+    return r
+
+
+print(surpasserCount([1,2,3,4,5]))
