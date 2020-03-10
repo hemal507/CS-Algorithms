82e53677b82e1fdc9da00839cd5da4745a2e6362
hemal507 <hchowdam@gmail.com>
2020-03-10 18:13:14 +0530

    isSum.py

--- isSum.py
+++ isSum.py
@@ -1,4 +1,9 @@
 def isSum(value):
-	return bool(([i for i in range(100) if sum(range(i)) == value]+[0])[0])
-
+	#return bool(([i for i in range(100) if sum(range(i)) == value]+[0])[0])
+	r=0
+	a=1
+	while value > r :
+		r+= a
+		a+=1
+	return value == r
 print(isSum(10))
