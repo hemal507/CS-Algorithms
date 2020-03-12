b8b80ce7ce2b4e7c0ebc952a1a6e7be1baf16043
hemal507 <hchowdam@gmail.com>
2020-03-12 22:45:49 +0530

    sumOfTwo.py

--- sumOfTwo.py
+++ sumOfTwo.py
@@ -1,9 +1,11 @@
-def sumOfTwo(a, b, v):
-    sa=set(a)
-    sb=set(b)
-    for i in sa :
-        if v-i in sb :
-            return True
-    return False
+# def sumOfTwo(a, b, v):
+#     sa=set(a)
+#     sb=set(b)
+#     for i in sa :
+#         if v-i in sb :
+#             return True
+#     return False
 
+
+sumOfTwo = lambda a, b, v : bool(set(a) & {v - i for i in b})
 
