f6923531e637410360f44dd92690735d0f9787a1
hemal507 <hchowdam@gmail.com>
2020-03-14 17:26:34 +0530

    isDiagonalMatrix.py

--- isDiagonalMatrix.py
+++ isDiagonalMatrix.py
@@ -1,7 +1,9 @@
 def isDiagonalMatrix(matrix):
     l=len(matrix)
-    for i in range(l) :
-        for j in range(l) :
-            if i != j and matrix[i][j] != 0 :
-                return False
-    return True
+    return not (any([matrix[x][y] != 0 and x != y for x in range(l) for y in range(l)]))
+    #
+    # for i in range(l) :
+    #     for j in range(l) :
+    #         if i != j and matrix[i][j] != 0 :
+    #             return False
+    # return True
