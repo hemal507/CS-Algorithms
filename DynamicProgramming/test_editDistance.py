d8db430dee00a59e64a3f716ed99c2959014ab67
hemal507 <hchowdam@gmail.com>
2020-05-21 17:37:03 +0530

    'update'

--- DynamicProgramming/test_editDistance.py
+++ DynamicProgramming/test_editDistance.py
@@ -1,17 +1,25 @@
 from DynamicProgramming import editDistance
 
+
 def test_case1():
-    assert editDistance.editDistance("cart","match") == 4
+    assert editDistance.editDistance("cart", "match") == 4
+
 
 def test_case2():
-    assert editDistance.editDistance("cart","cart") == 0
+    assert editDistance.editDistance("cart", "cart") == 0
+
 
 def test_case3():
-    assert editDistance.editDistance("a","") == 1
+    assert editDistance.editDistance("a", "") == 1
+
 
 def test_case4():
-    assert editDistance.editDistance("","a") == 1
+    assert editDistance.editDistance("", "a") == 1
+
 
 def test_case5():
-    assert editDistance.editDistance("CART","cart") == 4
+    assert editDistance.editDistance("CART", "cart") == 4
 
+
+def test_case6():
+    assert editDistance.editDistance("abcfg", "adceg") == 2
